11435db42d802152ae82b02d7d07d969eaf9ca40
Jijeong Lee <malgamlee@gmail.com>
2022-05-01 11:07:23 +0900

    백준 11279 최대 힙 - 말감

---
+++ 2022-05-01/baekjoon_11279_malgam.py
@@ -0,0 +1,18 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+arr = []
+
+for _ in range(n) :
+    num = int(input())
+    if num != 0 :
+        heapq.heappush(arr, -num)
+    else :
+        if len(arr) == 0 :
+            print(0)
+        else :
+            popNum = heapq.heappop(arr)
+            print(-popNum)
