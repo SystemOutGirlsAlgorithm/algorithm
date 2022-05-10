a5edc77c7f299c6ea11ef53f1180e6994aa8a63b
JIEunKim/김지은 <71249556+EssLemint@users.noreply.github.com>
2022-05-10 23:58:36 +0900

    Update and rename Baekjoon10818LM.java to Baekjoon_10818_LM.java

--- 2022-05-10/Baekjoon_10818_LM.java
+++ 2022-05-10/Baekjoon_10818_LM.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Baekjoon10818LM {
-ㄴ
+
   public static void main(String[] args) throws  Exception{
     /**  input , 숫자 하나하나 받는 방법
      * */
@@ -17,4 +17,4 @@ public class Baekjoon10818LM {
    System.out.print("최소 : " + numbers[0] + ", 최대 : " + numbers[numbers.length - 1]);
   }
 
-}ㄴ
+}
