3a87a86e0643c7ca626d3fa4cd2afb4d597717f7
drowned <87066883+drown-ed@users.noreply.github.com>
2022-05-16 16:53:39 +0900

    백준 2110 공유기 설치 - Jedi

--- 2022-05-16/baekjoon_2110_jedi.py
+++ 2022-05-16/baekjoon_2110_jedi.py
@@ -1,3 +1,40 @@
+<<<<<<< HEAD
+import sys
+input = sys.stdin.readline
+
+n, c = map(int, input().split())
+arr = []
+for _ in range(n):
+    arr.append(int(input()))
+
+arr.sort()
+
+end = arr[-1] - arr[0]
+start = 1
+ans = 0
+
+while start <= end:
+    mid = (start + end) // 2
+    count = 1
+    current_router = arr[0]
+    tmp = float("INF")
+
+    for i in range(1, n):
+        if current_router + mid <= arr[i]:
+            tmp = min(arr[i] - current_router, tmp)
+            count += 1
+            current_router = arr[i]
+
+    if count < c: # 공유기 설치를 더 해야함 -> 간격을 짧게 해야 함
+        end = mid - 1
+
+    elif count >= c: # 공유기 설치가 완료 or 더 많이 됨 -> 간격 늘려야 함
+        start = mid + 1
+        ans = max(ans, tmp)
+    
+
+print(ans)
+=======
 #baekjoon_2110_jedi
 #binary search
 
@@ -35,3 +72,4 @@ answer = 0
 
 binary_search(array, start, end)
 print(answer)
+>>>>>>> 9450005937666a24261dbf7e7ea401d50acbde02
