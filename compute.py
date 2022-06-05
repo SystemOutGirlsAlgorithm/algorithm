3b5dc811126bc99bd44f5b1abcfc553ee9bda6fe
mimseong <parkseongmin98@gmail.com>
2022-06-05 23:21:32 +0900

    Feat: 시작일 인자 하나만 받게 변경

--- compute.py
+++ compute.py
@@ -3,17 +3,30 @@ import re
 import sys
 import os
 
-member = ["말감", "진도", "다니", "dukdn", "Yoou", "Marvy", "Muer", "깜콩쥬", "미도리", "YUN", "Beankong", "안잉", "라임민트", "유감", "Jedi", "OoO", "룰루"]
+member = ["말감", "진도", "Yoou", "Marvy", "Muer", "미도리", "Beankong", "안잉", "라임민트", "유감", "Jedi", "OoO", "룰루", "yoon", "dyno0o", "한딱", "Soyou", "서나"]
 problem_count = {}
 
-def get_time_range_list(month, to_day, from_day, file_list):
-    # 조금 더 현명하게 날짜를 계산하는 방법이 필요
-    pattern = re.compile('2022-{0}-2[{1}-{2}]'.format(month, to_day, from_day))
-    return list(filter(lambda file: pattern.match(file) , file_list))
+class Date:
+    def __init__(self, date: str):
+        if self.check_date_format(date, '\d\d\d\d-\d\d-\d\d') == False:
+            raise ValueError('yyyy-mm-dd 형식을 맞춰주세요')
+        self.year = int(date[0:4])
+        self.month = int(date[5:7])
+        self.day = int(date[8:10])
+    
+    def __str__(self):
+        return self.year + "-" + self.month + "-" + self.day
+    
+    def check_date_format(self, input: str, format: str):
+        if re.compile(format).match(input) == None:
+            return False
+        return True
 
-def check_day_member(day):
-    file_list = os.listdir("./{0}".format(day))
+def check_day_member(day: dt.date):
+    file_list = os.listdir("./{0}".format(day.strftime("%Y-%m-%d")))
     for file in file_list:
+        if file == ".DS_Store":
+            continue
         isMember = False
         for m in member:
             if m in file:
@@ -24,17 +37,30 @@ def check_day_member(day):
         if not isMember:
             print("없는 멤버입니다 {0}".format(file))
 
-def main():
-    month, to_day, from_day = sys.argv[1], sys.argv[2], sys.argv[3]
-    print(month, to_day, from_day)
+def input_date():
+    to_date = sys.argv[1]
+    return to_date
 
-    file_list = os.listdir("./")
-    time_range_list = get_time_range_list(month, to_day, from_day, file_list)
-    for day in time_range_list:
+def get_date_list(date: dt.date, days: int):
+    lst = [date]
+    for i in range(days):
+        date += dt.timedelta(days=1)
+        lst.append(date)
+    return lst
+
+def main():
+    to_date_str = input_date()
+    to_date = Date(to_date_str)
+
+    t = dt.date(to_date.year, to_date.month, to_date.day)
+    date_list = get_date_list(t, 6)
+    for day in date_list:
         check_day_member(day)
     print(problem_count)
 
 
 if __name__ == '__main__':
-  if len(sys.argv) == 4:
+  if len(sys.argv) == 2:
     main()
+  else:
+    print('compute.py yyyy-mm-dd')
