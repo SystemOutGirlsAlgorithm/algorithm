76f4bc50653c29b62ec52b87f5f68830cd0abeec
HyeWon Kim <76997276+haeburney@users.noreply.github.com>
2022-06-23 13:34:21 +0900

    백준 2747 피보나치 수 - 한딱

--- 2022-06-23/baekjoon_2747_한딱.java
+++ 2022-06-23/baekjoon_2747_한딱.java
@@ -1,10 +1,8 @@
-package baekjoon;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class fibonacci_2747 {
+public class Main {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		
@@ -21,6 +19,4 @@ public class fibonacci_2747 {
 		}
 		System.out.print(sum);
 	}
-	
-	
 }
