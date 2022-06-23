3086a163564848828a5bd962f1b7698f17486bbc
HyeWon Kim <76997276+haeburney@users.noreply.github.com>
2022-06-23 13:33:39 +0900

    Update baekjoon_2747_한딱.java

--- 2022-06-23/baekjoon_2747_한딱.java
+++ 2022-06-23/baekjoon_2747_한딱.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class fibonacci_11444 {
+public class fibonacci_2747 {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		
