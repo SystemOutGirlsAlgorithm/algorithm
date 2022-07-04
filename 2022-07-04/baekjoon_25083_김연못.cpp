c02fdb30443e7955d6b6848878283a5a1a5de197
t1g2r <262999@naver.com>
2022-07-04 23:14:25 +0900

    백준 25083 새싹 - 김연못

---
+++ 2022-07-04/baekjoon_25083_김연못.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        cout << "         ,r'\"7" << endl;
+        cout << "r`-_   ,'  ,/" << endl;
+        cout << " \\. \". L_r'" << endl;
+        cout << "   `~\\/" << endl;
+        cout << "      |" << endl;
+        cout << "      |" << endl;
+
+        
+}
