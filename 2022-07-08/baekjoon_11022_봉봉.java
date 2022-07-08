5ab26a9da86e49307ed98cfa8e63b139f1c5c849
onyoo <dhsdb02@gmail.com>
2022-07-08 20:08:07 +0900

    baekjoon 11022 A+B -8 봉봉

---
+++ 2022-07-08/baekjoon_11022_봉봉.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(bf.readLine());
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            bw.write(String.format("Case #%d: %d + %d = %d", i + 1, a, b, a + b) + "\n");
+        }
+
+        bw.close();
+
+    }
+}
