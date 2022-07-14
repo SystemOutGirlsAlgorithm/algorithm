c297722b1e08187acab8dc25cc95c3f6344bb779
malgamlee <malgamlee@gmail.com>
2022-07-14 19:04:21 +0900

    fix. compute.py 멤버 이름 변경, 파일 확장자 추가

--- compute.py
+++ compute.py
@@ -3,8 +3,8 @@ import os
 import re
 import sys
 
-MEMBER_LIST = ["말감", "진도", "Yoou", "Marvy",  "안잉", "라임민트", "dyno0o", "서나", "김연못", "Hyun", "집", "가지덮밥", "차미", "파도", "yeonjiyeon", "봉봉", "ramu", "두괴풋"]
-SOURCE_FILE_EXTENSION = [".java", ".cpp", ".js", ".scala", ".py", ".c"]
+MEMBER_LIST = ["말감", "진도", "Yoou", "Marvy",  "안잉", "라임민트", "dyno0o", "서나", "김연못", "hyun", "집", "가지덮밥", "차미", "파도", "yeonjiyeon", "봉봉", "ramu", "두괴풋"]
+SOURCE_FILE_EXTENSION = [".java", ".cpp", ".js", ".scala", ".py", ".c", ".ipynb"]
 
 problem_count = {}
 
