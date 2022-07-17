db988084e80bf7d020562e5ef3a290d3eef9c5b4
t1g2r <262999@naver.com>
2022-07-17 23:35:13 +0900

    백준 16486 운동장 한 바퀴 - 김연못

---
+++ 2022-07-17/baekjoon_16486_김연못.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+#define PI 3.141592
+int main() {
+	fastio;
+	int d1;
+	int d2;
+	cin >> d1;
+	cin >> d2;
+
+	cout << fixed << setprecision(6) << (d1 * 2) + (2 * PI * d2) << '\n';
+
+}
