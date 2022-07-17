f87cd6c10ab4f2f471637ea26d9632e881656572
t1g2r <262999@naver.com>
2022-07-17 23:32:39 +0900

    백준 11023 더하기 3 - 김연못

---
+++ 2022-07-17/baekjoon_11023_김연못.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+
+int main() {
+
+	fastio;
+	int n;
+	int sum = 0;
+
+	
+	while (cin >> n)
+	{
+		sum = sum + n;
+	}
+	cout << sum << '\n';
+}
