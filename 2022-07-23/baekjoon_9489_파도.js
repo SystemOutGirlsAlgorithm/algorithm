8efe5ffb331f8fa9b2432d3401aa569e2fa12d53
MIINII <83333217+MIINII@users.noreply.github.com>
2022-07-23 19:06:16 +0900

    백준 시험성적 - 파도

---
+++ 2022-07-23/baekjoon_9489_파도.js
@@ -0,0 +1,13 @@
+function test(score) {
+  if (90 <= score <= 100) {
+    console.log('A')
+  } else if (80 <= score <= 89) {
+    console.log('B')
+  } else if (70 <= score <= 79) {
+    console.log('c')
+  } else if (60 <= score <= 60) {
+    console.log('d')
+  }
+}
+
+test(100)
