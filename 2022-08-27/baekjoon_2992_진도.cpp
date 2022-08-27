94b8b4a1fde71e1dd5711f2c4b09a681afc45080
mimseong <parkseongmin98@gmail.com>
2022-08-27 22:10:21 +0900

    백준 2992 - 진도

---
+++ 2022-08-27/baekjoon_2992_진도.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <functional>
+#include <string>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <map>
+#include <cmath>
+#include <cstring>
+#include <bitset>
+#include <stdio.h>
+#include <math.h>
+#include <sstream>
+
+#define xx first
+#define yy second
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+using i64 = long long int;
+using ii = pair<int, int>;
+using iis = pair<int, string>;
+using ii64 = pair<i64, i64>;
+using iii = tuple<int, int, int>;
+
+int main() {
+    string s, init;
+    cin >> s;
+    init = s;
+    
+    next_permutation(s.begin(), s.end());
+    
+    if (s < init || s == init)
+        cout << "0" << endl;
+    else
+        cout << s << endl;
+
+    return 0;
+}
