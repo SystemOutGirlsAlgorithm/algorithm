332cf07a60fc3873e07b9fbf474a442e3fe338eb
Yujin Cho <sysojxj@gmail.com>
2022-09-11 23:44:36 +0900

    백준 10926 ??! - jxx

---
+++ 2022-09-11/baekjoon_10926_jxx.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Baekjoon_10926 {
+    public static void main(String[] args){
+        Scanner str = new Scanner(System.in);
+        String id = str.nextLine();
+        String wow = "??!";
+        String result = id + wow;
+        System.out.println(result);
+    }
+}
