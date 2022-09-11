9f7b77b93fa7116bec22186c6b80c6f3ad3625a5
Yujin Cho <sysojxj@gmail.com>
2022-09-11 23:43:34 +0900

    백준 18108 1998년생인 내가 태국에서는 2541년생?! - jxx

---
+++ 2022-09-11/baekjoon_18108_jxx.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Baekjoon_18108 {
+    public static void main(String[] args){
+        Scanner bud = new Scanner(System.in);
+        int y = bud.nextInt();
+        int x = 543;
+        int result = y - x;
+        System.out.println(result);
+    }
+}
