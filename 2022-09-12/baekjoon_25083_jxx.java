70b0f585600385be55203b99e6dc38a144300f3c
Yujin Cho <sysojxj@gmail.com>
2022-09-12 17:31:31 +0900

    백준 25083 새싹 - jxx

---
+++ 2022-09-12/baekjoon_25083_jxx.java
@@ -0,0 +1,10 @@
+public class Baekjoon_25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
