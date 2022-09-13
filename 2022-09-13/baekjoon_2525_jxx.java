a5d257b23fbeb925102d26576c80bef01e26ad9b
Yujin Cho <sysojxj@gmail.com>
2022-09-13 18:07:13 +0900

    백준 2525 오븐 시계 - jxx

---
+++ 2022-09-13/baekjoon_2525_jxx.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Baekjoon_2525 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        int min = 60 * A + B;
+        min += C;
+
+        int hour = (min / 60) % 24;
+        int minute = min % 60;
+
+        System.out.println(hour + " " + minute);
+    }
+}
