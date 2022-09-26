cd5a83a469a323095edd9e0e89b03e4fcd634304
Yujin Cho <sysojxj@gmail.com>
2022-09-27 04:01:31 +0900

    백준 4344 평균은 넘겠지 - jxx

---
+++ 2022-09-27/baekjoon_4344_jxx.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class baekjoon_4344_jxx {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int C = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int sum = 0;
+        int over = 0;
+
+        for (int i = 0; i < C; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int[] arr = new int[a];
+            for (int j = 0; j < a; j++){
+                arr[j] = Integer.parseInt(st.nextToken());
+                sum += arr[j];
+            }
+            double avg = (double) sum / a;
+            for (int k = 0; k < a; k++){
+                if(arr[k] > avg){
+                    over++;
+                }
+            }
+            double ratio = (double) over / a * 100;
+            String ratio2 = String.format("%.3f", ratio);
+            System.out.println(ratio2 + "%");
+
+            sum = 0;
+            over = 0;
+        }
+    }
+}
