2d0937f96aa1bc72a8228a6c558b5181aed8e2a9
ph-value <phvalue123@gmail.com>
2022-09-30 20:26:40 +0900

    leetcode 27 - 산도

---
+++ 2022-09-30/leetcode_27_산도.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        
+        for( int i = 0 ; i < nums.length ; i++ ){
+            if( nums[i] != val ){
+                nums[k++] = nums[i];
+            }
+        }
+        
+        return k;
+    }
+}
