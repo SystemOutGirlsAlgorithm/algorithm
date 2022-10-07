35393a793bad4593a5ad31abc39074449fa1dbda
jieun-p974 <jieun.p974@gmail.com>
2022-10-08 01:39:24 +0900

    1008팥밥 수정

--- 10월/2022-10-08/programmers_비밀지도_팥밥.js
+++ 10월/2022-10-08/programmers_비밀지도_팥밥.js
@@ -1,15 +1,7 @@
-function solution(n, arr1, arr2) {
-  const result = [];
-
-  for (let i = 0; i < n; i++) {
-    let num = (arr1[i] | arr2[i]).toString(2);
-    num = num.replace(/1/g, "#").replace(/0/g, " ");
-    result.push(num.padStart(n, " "));
-  }
-
-  return result;
-}
-
+// 해결 못해서 다른 사람 풀이 참고
+//방법 : map을 사용해서 각 배열 a와 b를 2진수로 만들고
+// padStart로 n만큼 0으로 채워둠(빈칸을 채움)
+// replace와 정규식을 사용해서 0은 공백으로 1은 #으로 바꿈
 var solution = (n, a, b) =>
   a.map((a, i) =>
     (a | b[i]).toString(2).padStart(n, 0).replace(/0/g, " ").replace(/1/g, "#")
