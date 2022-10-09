22c7141e00b1ee739352b007c62c35a0f19ad650
hgyuhyeon <hwgyuhyeon@gmail.com>
2022-10-09 17:58:54 +0900

    leetcode 509 - 밍

---
+++ 10월/2022-10-09/leetcode_509_밍.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n < 1)
+            return 0;
+        else if (n == 1)
+            return 1;
+        else
+            return fib(n - 1) + fib(n - 2);
+    }
+};
