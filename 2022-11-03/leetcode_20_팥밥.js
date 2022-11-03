7ec1914ecde6be7d92f536b7a2871334845e22db
jieun-p974 <jieun.p974@gmail.com>
2022-11-03 16:01:19 +0900

    leetcode 20.Valid parentheses

---
+++ 2022-11-03/leetcode_20_팥밥.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    let c = s.charAt(i);
+
+    switch (c) {
+      case "(":
+        stack.push(")");
+        break;
+      case "{":
+        stack.push("}");
+        break;
+      case "[":
+        stack.push("]");
+        break;
+      default:
+        if (c !== stack.pop()) {
+          return false;
+        }
+    }
+  }
+  return stack.length === 0;
+};
