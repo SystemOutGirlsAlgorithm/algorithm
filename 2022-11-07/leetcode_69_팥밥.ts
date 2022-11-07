24a4922deb63664b3f726bea7648293d2dd60537
jieun-p974 <jieun.p974@gmail.com>
2022-11-07 22:59:27 +0900

    leetcode 69.sqrt(x) 팥밥

---
+++ 2022-11-07/leetcode_69_팥밥.ts
@@ -0,0 +1,19 @@
+function mySqrt(x: number): number {
+  var left = 1;
+  var right = Math.floor(x / 2) + 1;
+  var mid;
+
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+
+    if (mid * mid > x) {
+      right = mid - 1;
+    } else if (mid * mid < x) {
+      left = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+
+  return right;
+}
