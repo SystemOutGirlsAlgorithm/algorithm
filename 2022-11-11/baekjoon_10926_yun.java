3dd91d61ba7999bf247127301c96230912bfa011
olzj-lb7 <wjddbsgml0126@gmail.com>
2022-11-11 16:06:51 +0900

    Create baekjoon_10926_yun.java
    백준 10926

---
+++ 2022-11-11/baekjoon_10926_yun.java
@@ -0,0 +1,14 @@
+package baekjoon_221111;
+
+import java.util.Scanner;
+
+public class B10926 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = "??!";
+		String b = sc.nextLine();
+		
+		System.out.println(b + a);
+	}
+}
+
