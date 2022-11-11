d54d4fc74344c25ea44a08a77eed9d165ddc8560
olzj-lb7 <wjddbsgml0126@gmail.com>
2022-11-11 16:11:14 +0900

    Create baekjoon_18108_yun.java
    백준 18108

---
+++ 2022-11-11/baekjoon_18108_yun.java
@@ -0,0 +1,13 @@
+package baekjoon_221111;
+
+import java.util.Scanner;
+
+public class B18108 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = 543;
+
+		System.out.println(sc.nextInt()-a);
+	}
+}
+
