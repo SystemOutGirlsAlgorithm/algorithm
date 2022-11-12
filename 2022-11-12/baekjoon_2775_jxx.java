3f33ae3924246712cfe9ed6627c57fe1127cb879
Yujin Cho <sysojxj@gmail.com>
2022-11-12 18:45:21 +0900

    Update README.md

---
+++ 2022-11-12/baekjoon_2775_jxx.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class baekjoon_2775_jxx {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[][] APT = new int[15][15];
+
+        for(int i = 0; i < 15; i++) {
+            APT[i][1] = 1;
+            APT[0][i] = i;
+        }
+
+        for(int i = 1; i < 15; i ++) {
+            for(int j = 2; j < 15; j++) {
+                APT[i][j] = APT[i][j - 1] + APT[i - 1][j];
+            }
+        }
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < T; i++) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(APT[k][n]);
+        }
+        br.close();
+    }
+}
