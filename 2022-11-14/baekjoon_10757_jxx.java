3c984684a952d42e2d4d4977d5daa43daef5a865
Yujin Cho <sysojxj@gmail.com>
2022-11-14 21:32:28 +0900

    백준 10757 큰 수 A+B - jxx

--- 2022-11-14/baekjoon_10757_jxx.java
+++ 2022-11-14/baekjoon_10757_jxx.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.util.StringTokenizer;
 // 큰 수 A+B
-public class Baekjoon_10757 {
+public class baekjoon_10757_jxx {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
