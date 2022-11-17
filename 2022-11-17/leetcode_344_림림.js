b2be35f9640abe63ece9f3f698d9cb518ae96337
selim-jo <skyblue.777@daum.net>
2022-11-17 20:37:49 +0900

    leetcode 344 - 림림

---
+++ 2022-11-17/leetcode_344_림림.js
@@ -0,0 +1,7 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+ var reverseString = function(s) {
+    return s.reverse(); // 배열 순서 거꾸로 뒤집을 때 사용(Array.reverse())
+};
