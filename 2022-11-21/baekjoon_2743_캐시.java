25039332f752817889c5c843085ea595197ba21c
SUYEON <ch2oyeon@gmail.com>
2022-11-21 22:52:21 +0900

    백준 2743 단어 길이 재기 - 캐시

---
+++ 2022-11-21/baekjoon_2743_캐시.java
@@ -0,0 +1,13 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//단어 길이 재기
+public class baekjoon_2743_캐시 {
+    public static void main(String[] args) throws IOException {
+        //1. 단어 길이 출력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(br.readLine().length());
+    }
+}
