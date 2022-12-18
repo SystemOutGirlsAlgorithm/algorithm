1e208d482df3c01e961817617187dbff39e9e1cd
donnyrla10 <kokoo1214@naver.com>
2022-12-18 12:06:43 +0900

    백준 3085 사탕 게임(수정) - 녕이

--- 2022-12-18/baekjoon_3085_녕이.swift
+++ 2022-12-18/baekjoon_3085_녕이.swift
@@ -9,73 +9,73 @@ import Foundation
  */
 
 //쓸데없는 연산/시간을 줄이기 위해 swap되어 변한 row, col만 체크
-func calculateMaximumNumberOfCandy(check1: Int, check2: Int, check3: Int, isRow: Bool, tmpArray: inout [[String]]) -> Int { //연속으로 이어진 같은 색의 사탕 개수 최댓값 구하기
+func calculateMaximumNumberOfCandy(check1: Int, check2: Int, check3: Int, isRow: Bool) -> Int { //연속으로 이어진 같은 색의 사탕 개수 최댓값 구하기
     var max = 0
     if isRow {
         var count = 1
-        var last = tmpArray[check1][0]
+        var last = array[check1][0]
         for i in 1..<N {
-            if last == tmpArray[check1][i] {
+            if last == array[check1][i] {
                 count += 1
                 max = max < count ? count : max //update maximum value
             }else {
-                last = tmpArray[check1][i]
+                last = array[check1][i]
                 count = 1
             }
         }
         count = 1
-        last = tmpArray[0][check2]
+        last = array[0][check2]
         for i in 1..<N {
-            if last == tmpArray[i][check2] {
+            if last == array[i][check2] {
                 count += 1
                 max = max < count ? count : max //update maximum value
             }else {
-                last = tmpArray[i][check2]
+                last = array[i][check2]
                 count = 1
             }
         }
         count = 1
-        last = tmpArray[0][check3]
+        last = array[0][check3]
         for i in 1..<N {
-            if last == tmpArray[i][check3] {
+            if last == array[i][check3] {
                 count += 1
                 max = max < count ? count : max //update maximum value
             }else {
-                last = tmpArray[i][check3]
+                last = array[i][check3]
                 count = 1
             }
         }
     }else {
         var count = 1
-        var last = tmpArray[check1][0]
+        var last = array[check1][0]
         for i in 1..<N {
-            if last == tmpArray[check1][i] {
+            if last == array[check1][i] {
                 count += 1
                 max = max < count ? count : max //update maximum value
             }else {
-                last = tmpArray[check1][i]
+                last = array[check1][i]
                 count = 1
             }
         }
         count = 1
-        last = tmpArray[0][check2]
+        last = array[0][check2]
         for i in 1..<N {
-            if last == tmpArray[i][check2] {
+            if last == array[i][check2] {
                 count += 1
                 max = max < count ? count : max //update maximum value
             }else {
-                last = tmpArray[i][check2]
+                last = array[i][check2]
                 count = 1
             }
         }
         count = 1
-        last = tmpArray[check3][0]
+        last = array[check3][0]
         for i in 1..<N {
-            if last == tmpArray[check3][i] {
+            if last == array[check3][i] {
                 count += 1
                 max = max < count ? count : max //update maximum value
             }else {
-                last = tmpArray[check3][i]
+                last = array[check3][i]
                 count = 1
             }
         }
@@ -118,23 +118,30 @@ for i in 0..<N {
     }
 }
 
+//전체 원소 순회
 if !earlyExit {
-    //전체 원소 순회
+    //가로 swap
     for i in 0..<N {
-        for j in 0..<N {
-            for k in 0..<4 { //swap할 원소
-                let dx = i + direction[k][0]
-                let dy = j + direction[k][1]
-                guard (0..<N) ~= dx, (0..<N) ~= dy else { continue } //범위 내에 있는 사탕만
-                guard array[dx][dy] != array[i][j] else { continue } //색이 서로 다른 사탕만
-                var tmp = array //copy array
-                let tmpCandy = tmp[dx][dy]
-                tmp[dx][dy] = tmp[i][j]         //swap
-                tmp[i][j] = tmpCandy            //swap
-                let result = (0...1) ~= k ? calculateMaximumNumberOfCandy(check1: i, check2: j, check3: dy, isRow: true, tmpArray: &tmp)
-                                          : calculateMaximumNumberOfCandy(check1: i, check2: j, check3: dx, isRow: false, tmpArray: &tmp)
-                answer = answer < result ? result : answer
-            }
+        for j in 0..<N - 1 {
+            guard array[i][j] != array[i][j+1] else { continue } //같은 색은 ㄴㄴ
+            array[i].swapAt(j, j+1)
+            let result = calculateMaximumNumberOfCandy(check1: i, check2: j, check3: j+1, isRow: true)
+            answer = answer < result ? result : answer
+            array[i].swapAt(j, j+1) //초기화
+        }
+    }
+    //세로 swap
+    for j in 0..<N {
+        for i in 0..<N - 1{
+            guard array[i][j] != array[i+1][j] else { continue } //같은 색은 ㄴㄴ
+            var tmp = array[i][j]
+            array[i][j] = array[i+1][j]
+            array[i+1][j] = tmp
+            let result = calculateMaximumNumberOfCandy(check1: i, check2: j, check3: i+1, isRow: false)
+            answer = answer < result ? result : answer
+            tmp = array[i][j]
+            array[i][j] = array[i+1][j]
+            array[i+1][j] = tmp
         }
     }
     print(answer)
