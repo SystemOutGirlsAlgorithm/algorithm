b08b1073d92792ca417bd2a8ff696ff2eadc9f5f
Yeon Kyung Ryu <celina16161616@gmail.com>
2022-12-25 17:46:22 +0900

    백준 10869 사칙연산 - celina

---
+++ 2022-12-25/baekjoon_10869_celina.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a;
+	cin >> b;
+
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+	cout << a % b << endl;
+
+	return 0;
+}
