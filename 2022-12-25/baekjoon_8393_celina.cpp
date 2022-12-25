5c9800c47d71649a263af4cd672a233e4a378890
Yeon Kyung Ryu <celina16161616@gmail.com>
2022-12-25 18:03:12 +0900

    백준 8393 합 - celina

---
+++ 2022-12-25/baekjoon_8393_celina.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	int sum = 0;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		sum += i;
+	}
+
+	cout << sum;
+
+
+	return 0;
+}
